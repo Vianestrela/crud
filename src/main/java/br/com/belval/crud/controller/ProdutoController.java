daa63838ebd2fdee1a925f1fbad58d51888d15a9
Vianestrela <vivianefdasilva@yahoo.com.br>
2023-09-22 20:53:11 -0300

    PathVariable

--- src/main/java/br/com/belval/crud/controller/ProdutoController.java
+++ src/main/java/br/com/belval/crud/controller/ProdutoController.java
@@ -4,7 +4,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -14,25 +16,51 @@ import br.com.belval.crud.model.Produto;
 public class ProdutoController {
 	
 	private static List<Produto> lista = new ArrayList<>();
-	private static int proxid = 1;
+	private static int proxId = 1;
+
 	@GetMapping("/produto/novo")
 	public String novo() {
 		return "novo-produto";
 	}
-		/*vivitest*/
+	
 	@PostMapping("/produto/novo")
 	public ModelAndView novo(Produto produto) {
 		ModelAndView modelAndView = new ModelAndView("novo-produto-criado");
-		produto.setId(proxid++);
+		
+		produto.setId(proxId++);
 		
 		lista.add(produto);
+		
 		modelAndView.addObject("novoProduto", produto);
 		return modelAndView;
 	}
-		@GetMapping ("/produto/list")
-		public ModelAndView list() {
-			ModelAndView modelAndView = new ModelAndView("lista-produtos");
-			modelAndView.addObject("produtos", lista);
-			return modelAndView;
+
+	@GetMapping("/produto/list")
+	public String list(Model model) {
+		model.addAttribute("produtos", lista);
+		return "lista-produtos";
+	}
+	@GetMapping("/produto/{id}")
+	
+	public String detalhe(@PathVariable int id, Model model) {
+		Produto encontrou = null;
+		for(Produto p : lista) {
+			if (p.getId() == id) {
+				//encontrou o produto solicitado
+				encontrou = p;
+				break;
+			}
 		}
+		
+		if (encontrou != null) {
+			model.addAttribute("novoProduto", encontrou);
+			return "novo-produto-criado";
+		}
+		
+		return "produto-nao-encontrado";
+	}
+	
+	
+	
+	
 }
